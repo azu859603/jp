05790000352a40bfbd7c5b7d5bbe090f0d76b4f9
ax202 <ax2020rush@gmail.com>
2026-08-30 16:42:40 +0800

    页面修改

--- app/lang/en-us.php
+++ app/lang/en-us.php
@@ -595,4 +595,5 @@ return [
     '语言' => 'Language',
     '实名认证审核中，请勿重复提交' => 'Verification under review, please do not resubmit',
     '您的入驻申请已提交，请耐心等待审核<br>（1-3 个工作日）' => 'Your application has been submitted, please wait<br>(1-3 working days)',
+    '全部分类' => 'All Categories',
 ];
